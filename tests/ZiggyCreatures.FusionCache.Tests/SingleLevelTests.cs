be770f72e4b7fb6a50ca23075655b9ae675675a5
Jody Donetti <indastria@gmail.com>
2022-02-19 09:52:57 +0100

    Minor

--- tests/ZiggyCreatures.FusionCache.Tests/SingleLevelTests.cs
+++ tests/ZiggyCreatures.FusionCache.Tests/SingleLevelTests.cs
@@ -28,7 +28,7 @@ namespace FusionCacheTests
 		{
 			Assert.Throws<ArgumentNullException>(() =>
 			{
-				var foo = new FusionCacheOptions() { DefaultEntryOptions = null };
+				var foo = new FusionCacheOptions() { DefaultEntryOptions = null! };
 			});
 		}
 
@@ -609,59 +609,5 @@ namespace FusionCacheTests
 				Assert.Equal(3, default3);
 			}
 		}
-
-		//[Fact]
-		//public async Task DirectEvictionWorksAsync()
-		//{
-		//	using (var cache = new FusionCache(new FusionCacheOptions() { EnableSyncEventHandlersExecution = true }))
-		//	{
-		//		var removeCalled = false;
-		//		EventHandler<FusionCacheEntryEventArgs> onRemove = (s, e) => removeCalled = true;
-
-		//		cache.Events.Memory.Remove += onRemove;
-
-		//		var duration = TimeSpan.FromMinutes(1);
-
-		//		// SET THE VALUE (WITH FAIL-SAFE ENABLED)
-		//		await cache.SetAsync("foo", 42, opt => opt.SetDuration(duration).SetFailSafe(true)).ConfigureAwait(false);
-
-		//		// EVICT
-		//		cache.Evict("foo", false);
-
-		//		var res = await cache.TryGetAsync<int>("foo", opt => opt.SetFailSafe(true)).ConfigureAwait(false);
-
-		//		cache.Events.Memory.Remove -= onRemove;
-
-		//		Assert.True(removeCalled);
-		//		Assert.False(res.HasValue);
-		//	}
-		//}
-
-		//[Fact]
-		//public void DirectEvictionWorks()
-		//{
-		//	using (var cache = new FusionCache(new FusionCacheOptions() { EnableSyncEventHandlersExecution = true }))
-		//	{
-		//		var removeCalled = false;
-		//		EventHandler<FusionCacheEntryEventArgs> onRemove = (s, e) => removeCalled = true;
-
-		//		cache.Events.Memory.Remove += onRemove;
-
-		//		var duration = TimeSpan.FromMinutes(1);
-
-		//		// SET THE VALUE (WITH FAIL-SAFE ENABLED)
-		//		cache.Set("foo", 42, opt => opt.SetDuration(duration).SetFailSafe(true));
-
-		//		// EVICT
-		//		cache.Evict("foo", false);
-
-		//		var res = cache.TryGet<int>("foo", opt => opt.SetFailSafe(true));
-
-		//		cache.Events.Memory.Remove -= onRemove;
-
-		//		Assert.True(removeCalled);
-		//		Assert.False(res.HasValue);
-		//	}
-		//}
 	}
 }
