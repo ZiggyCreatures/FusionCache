ad90cace44281c95e0814028560d41535ef8db12
Jody Donetti <indastria@gmail.com>
2022-04-03 16:37:58 +0200

    Better nullability

--- src/ZiggyCreatures.FusionCache.Serialization.NewtonsoftJson/FusionCacheNewtonsoftJsonSerializer.cs
+++ src/ZiggyCreatures.FusionCache.Serialization.NewtonsoftJson/FusionCacheNewtonsoftJsonSerializer.cs
@@ -23,35 +23,31 @@ namespace ZiggyCreatures.Caching.Fusion.Serialization.NewtonsoftJson
 		private JsonSerializerSettings? Settings;
 
 		/// <inheritdoc />
-		public byte[] Serialize<T>(T obj)
+		public byte[] Serialize<T>(T? obj)
 		{
 #pragma warning disable CS8604 // Possible null reference argument.
-#pragma warning disable HAA0601 // Value type to reference type conversion causing boxing allocation
 			return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, Settings));
-#pragma warning restore HAA0601 // Value type to reference type conversion causing boxing allocation
 #pragma warning restore CS8604 // Possible null reference argument.
 		}
 
 		/// <inheritdoc />
-		public T Deserialize<T>(byte[] data)
+		public T? Deserialize<T>(byte[] data)
 		{
-#pragma warning disable CS8603 // Possible null reference return.
 			return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data), Settings);
-#pragma warning restore CS8603 // Possible null reference return.
 		}
 
 		/// <inheritdoc />
-		public ValueTask<byte[]> SerializeAsync<T>(T obj)
+		public ValueTask<byte[]> SerializeAsync<T>(T? obj)
 		{
 			// TODO: DO BETTER HERE
 			return new ValueTask<byte[]>(Serialize<T>(obj));
 		}
 
 		/// <inheritdoc />
-		public ValueTask<T> DeserializeAsync<T>(byte[] data)
+		public ValueTask<T?> DeserializeAsync<T>(byte[] data)
 		{
 			// TODO: DO BETTER HERE
-			return new ValueTask<T>(Deserialize<T>(data));
+			return new ValueTask<T?>(Deserialize<T>(data));
 		}
 	}
 
