7fe94d4884af5d8e127011b5c8a0e81162e06fd0
Jody Donetti <indastria@gmail.com>
2022-11-04 22:43:22 +0100

    Add DI ext methods

---
+++ src/ZiggyCreatures.FusionCache.Serialization.ProtoBufNet/FusionCacheNeueccMessagePackSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ProtoBuf.Meta;
+using ZiggyCreatures.Caching.Fusion.Serialization;
+using ZiggyCreatures.Caching.Fusion.Serialization.ProtoBufNet;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	/// <summary>
+	/// Extension methods for setting up FusionCache related services in an <see cref="IServiceCollection" />.
+	/// </summary>
+	public static class FusionCacheNeueccMessagePackSerializerServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Adds an implementation of <see cref="IFusionCacheSerializer"/> which uses the most used .NET ProtoBuf serializer, by Marc Gravell.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+		/// <param name="model">The <see cref="RuntimeTypeModel"/> to use: if not specified, the default one (<see cref="RuntimeTypeModel.Default"/>) will be used.</param>
+		/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+		public static IServiceCollection AddFusionCacheProtoBufNetSerializer(this IServiceCollection services, RuntimeTypeModel? model = null)
+		{
+			if (services is null)
+				throw new ArgumentNullException(nameof(services));
+
+			services.TryAddSingleton<IFusionCacheSerializer>(_ => new FusionCacheProtoBufNetSerializer(model));
+
+			return services;
+		}
+	}
+}
