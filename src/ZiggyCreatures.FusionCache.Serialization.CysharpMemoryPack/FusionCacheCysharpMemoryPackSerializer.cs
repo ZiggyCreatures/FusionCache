0154cfd164344358bc1e6eb043623eb3f6a27c72
Jody Donetti <indastria@gmail.com>
2022-11-28 21:25:39 +0100

    Better serializer setup

--- src/ZiggyCreatures.FusionCache.Serialization.CysharpMemoryPack/FusionCacheCysharpMemoryPackSerializer.cs
+++ src/ZiggyCreatures.FusionCache.Serialization.CysharpMemoryPack/FusionCacheCysharpMemoryPackSerializer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MemoryPack;
 using ZiggyCreatures.Caching.Fusion.Internals.Distributed;
 using ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack.Internals;
@@ -13,28 +11,10 @@ namespace ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack
 	public class FusionCacheCysharpMemoryPackSerializer
 		: IFusionCacheSerializer
 	{
-
-		private static class Check<TGenericDistributedEntry>
+		static FusionCacheCysharpMemoryPackSerializer()
 		{
-			private static readonly Type FusionCacheDistributedEntrySurrogateType = typeof(FusionCacheDistributedEntrySurrogate<>);
-			static Check()
-			{
-				try
-				{
-					var tvalueType = typeof(TGenericDistributedEntry).GetGenericArguments()[0];
-					var b = FusionCacheDistributedEntrySurrogateType.MakeGenericType(tvalueType).GetMethod(nameof(FusionCacheDistributedEntrySurrogate<bool>.RegisterFormatter), BindingFlags.Static | BindingFlags.Public);
-					b?.Invoke(null, null);
-				}
-				catch
-				{
-					// EMPTY
-				}
-			}
-
-			public static void Ensure()
-			{
-				// EMPTY
-			}
+			FusionCacheEntryMetadataSurrogate.RegisterFormatter();
+			MemoryPackFormatterProvider.RegisterGenericType(typeof(FusionCacheDistributedEntry<>), typeof(FusionCacheDistributedEntryFormatter<>));
 		}
 
 		/// <summary>
@@ -44,58 +24,19 @@ namespace ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack
 		public FusionCacheCysharpMemoryPackSerializer(MemoryPackSerializerOptions? options = null)
 		{
 			Options = options;
-
-			FusionCacheEntryMetadataSurrogate.RegisterFormatter();
 		}
-
-		private static readonly Type _distributedEntryOpenGenericType = typeof(FusionCacheDistributedEntry<>);
 
 		private readonly MemoryPackSerializerOptions? Options;
-
-		private void MaybeRegisterDistributedEntryFormatter<T>()
-		{
-			var t = typeof(T);
-
-			if (t.IsGenericType == false || t.GetGenericTypeDefinition() != _distributedEntryOpenGenericType)
-				return;
-
-			Check<T>.Ensure();
-		}
-
-		//private static void TryRegisterDistributedEntryFormatter<TValue>()
-		//{
-		//	try
-		//	{
-		//		if (MemoryPackFormatterProvider.IsRegistered<FusionCacheDistributedEntry<TValue>>() == false)
-		//		{
-		//			var formatter = new FusionCacheDistributedEntryFormatter<TValue>();
-		//			MemoryPackFormatterProvider.Register(formatter);
-		//		}
-
-		//		if (MemoryPackFormatterProvider.IsRegistered<FusionCacheDistributedEntry<TValue>[]>() == false)
-		//		{
-		//			MemoryPackFormatterProvider.Register(new ArrayFormatter<FusionCacheDistributedEntry<TValue>>());
-		//		}
-		//	}
-		//	catch
-		//	{
-		//		// EMPTY
-		//	}
-		//}
 
 		/// <inheritdoc />
 		public byte[] Serialize<T>(T? obj)
 		{
-			MaybeRegisterDistributedEntryFormatter<T>();
-
 			return MemoryPackSerializer.Serialize<T>(obj, Options);
 		}
 
 		/// <inheritdoc />
 		public T? Deserialize<T>(byte[] data)
 		{
-			MaybeRegisterDistributedEntryFormatter<T>();
-
 			return MemoryPackSerializer.Deserialize<T?>(data, Options);
 		}
 
