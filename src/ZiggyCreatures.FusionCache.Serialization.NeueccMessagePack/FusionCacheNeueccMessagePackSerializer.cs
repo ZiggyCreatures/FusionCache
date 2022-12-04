a0f1e1ce258bd899d68e30f4b42ef399a85eef50
Jody Donetti <indastria@gmail.com>
2022-12-04 21:08:55 +0100

    Switch to c# 10 + file-scoped namespaces

--- src/ZiggyCreatures.FusionCache.Serialization.NeueccMessagePack/FusionCacheNeueccMessagePackSerializer.cs
+++ src/ZiggyCreatures.FusionCache.Serialization.NeueccMessagePack/FusionCacheNeueccMessagePackSerializer.cs
@@ -2,50 +2,49 @@
 using MessagePack;
 using MessagePack.Resolvers;
 
-namespace ZiggyCreatures.Caching.Fusion.Serialization.NeueccMessagePack
+namespace ZiggyCreatures.Caching.Fusion.Serialization.NeueccMessagePack;
+
+/// <summary>
+/// An implementation of <see cref="IFusionCacheSerializer"/> which uses Neuecc's famous MessagePack serializer.
+/// </summary>
+public class FusionCacheNeueccMessagePackSerializer
+	: IFusionCacheSerializer
 {
 	/// <summary>
-	/// An implementation of <see cref="IFusionCacheSerializer"/> which uses Neuecc's famous MessagePack serializer.
+	/// Create a new instance of a <see cref="FusionCacheNeueccMessagePackSerializer"/> object.
 	/// </summary>
-	public class FusionCacheNeueccMessagePackSerializer
-		: IFusionCacheSerializer
+	/// <param name="options">The <see cref="MessagePackSerializerOptions"/> to use: if not specified, the contract-less (<see cref="ContractlessStandardResolver"/>) options will be used.</param>
+	public FusionCacheNeueccMessagePackSerializer(MessagePackSerializerOptions? options = null)
 	{
-		/// <summary>
-		/// Create a new instance of a <see cref="FusionCacheNeueccMessagePackSerializer"/> object.
-		/// </summary>
-		/// <param name="options">The <see cref="MessagePackSerializerOptions"/> to use: if not specified, the contract-less (<see cref="ContractlessStandardResolver"/>) options will be used.</param>
-		public FusionCacheNeueccMessagePackSerializer(MessagePackSerializerOptions? options = null)
-		{
-			// PER @neuecc 'S SUGGESTION: DEFAULT TO THE CONTRACTLESS RESOLVER
-			Options = options ?? ContractlessStandardResolver.Options;
-		}
+		// PER @neuecc 'S SUGGESTION: DEFAULT TO THE CONTRACTLESS RESOLVER
+		Options = options ?? ContractlessStandardResolver.Options;
+	}
 
-		private readonly MessagePackSerializerOptions? Options;
+	private readonly MessagePackSerializerOptions? Options;
 
-		/// <inheritdoc />
-		public byte[] Serialize<T>(T? obj)
-		{
-			return MessagePackSerializer.Serialize<T?>(obj, Options);
-		}
+	/// <inheritdoc />
+	public byte[] Serialize<T>(T? obj)
+	{
+		return MessagePackSerializer.Serialize<T?>(obj, Options);
+	}
 
-		/// <inheritdoc />
-		public T? Deserialize<T>(byte[] data)
-		{
-			return MessagePackSerializer.Deserialize<T?>(data, Options);
-		}
+	/// <inheritdoc />
+	public T? Deserialize<T>(byte[] data)
+	{
+		return MessagePackSerializer.Deserialize<T?>(data, Options);
+	}
 
-		/// <inheritdoc />
-		public ValueTask<byte[]> SerializeAsync<T>(T? obj)
-		{
-			// PER @neuecc 'S SUGGESTION: AVOID AWAITING ON A MEMORY STREAM
-			return new ValueTask<byte[]>(Serialize(obj));
-		}
+	/// <inheritdoc />
+	public ValueTask<byte[]> SerializeAsync<T>(T? obj)
+	{
+		// PER @neuecc 'S SUGGESTION: AVOID AWAITING ON A MEMORY STREAM
+		return new ValueTask<byte[]>(Serialize(obj));
+	}
 
-		/// <inheritdoc />
-		public ValueTask<T?> DeserializeAsync<T>(byte[] data)
-		{
-			// PER @neuecc 'S SUGGESTION: AVOID AWAITING ON A MEMORY STREAM
-			return new ValueTask<T?>(Deserialize<T>(data));
-		}
+	/// <inheritdoc />
+	public ValueTask<T?> DeserializeAsync<T>(byte[] data)
+	{
+		// PER @neuecc 'S SUGGESTION: AVOID AWAITING ON A MEMORY STREAM
+		return new ValueTask<T?>(Deserialize<T>(data));
 	}
 }
--- src/ZiggyCreatures.FusionCache.Serialization.NeueccMessagePack/FusionCacheNeueccMessagePackSerializerServiceCollectionExtensions.cs
+++ src/ZiggyCreatures.FusionCache.Serialization.NeueccMessagePack/FusionCacheNeueccMessagePackSerializerServiceCollectionExtensions.cs
@@ -5,27 +5,26 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using ZiggyCreatures.Caching.Fusion.Serialization;
 using ZiggyCreatures.Caching.Fusion.Serialization.NeueccMessagePack;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for setting up FusionCache related services in an <see cref="IServiceCollection" />.
+/// </summary>
+public static class FusionCacheNeueccMessagePackSerializerServiceCollectionExtensions
 {
 	/// <summary>
-	/// Extension methods for setting up FusionCache related services in an <see cref="IServiceCollection" />.
+	/// Adds an implementation of <see cref="IFusionCacheSerializer"/> based on the famous Neuecc's MessagePack one.
 	/// </summary>
-	public static class FusionCacheNeueccMessagePackSerializerServiceCollectionExtensions
+	/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+	/// <param name="options">The <see cref="MessagePackSerializerOptions"/> to use: if not specified, the contract-less (<see cref="ContractlessStandardResolver"/>) options will be used.</param>
+	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+	public static IServiceCollection AddFusionCacheNeueccMessagePackSerializer(this IServiceCollection services, MessagePackSerializerOptions? options = null)
 	{
-		/// <summary>
-		/// Adds an implementation of <see cref="IFusionCacheSerializer"/> based on the famous Neuecc's MessagePack one.
-		/// </summary>
-		/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
-		/// <param name="options">The <see cref="MessagePackSerializerOptions"/> to use: if not specified, the contract-less (<see cref="ContractlessStandardResolver"/>) options will be used.</param>
-		/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-		public static IServiceCollection AddFusionCacheNeueccMessagePackSerializer(this IServiceCollection services, MessagePackSerializerOptions? options = null)
-		{
-			if (services is null)
-				throw new ArgumentNullException(nameof(services));
+		if (services is null)
+			throw new ArgumentNullException(nameof(services));
 
-			services.TryAddSingleton<IFusionCacheSerializer>(_ => new FusionCacheNeueccMessagePackSerializer(options));
+		services.TryAddSingleton<IFusionCacheSerializer>(_ => new FusionCacheNeueccMessagePackSerializer(options));
 
-			return services;
-		}
+		return services;
 	}
 }
