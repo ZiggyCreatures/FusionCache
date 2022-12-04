802c16f4b0c2831a438e49587284c7d4f71999a6
Jody Donetti <indastria@gmail.com>
2022-12-04 21:08:59 +0100

    Switch to c# 10 + file-scoped namespaces

--- src/ZiggyCreatures.FusionCache.Serialization.NewtonsoftJson/FusionCacheNewtonsoftJsonSerializer.cs
+++ src/ZiggyCreatures.FusionCache.Serialization.NewtonsoftJson/FusionCacheNewtonsoftJsonSerializer.cs
@@ -2,51 +2,50 @@
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace ZiggyCreatures.Caching.Fusion.Serialization.NewtonsoftJson
+namespace ZiggyCreatures.Caching.Fusion.Serialization.NewtonsoftJson;
+
+/// <summary>
+/// An implementation of <see cref="IFusionCacheSerializer"/> which uses the Newtonsoft Json.NET serializer.
+/// </summary>
+public class FusionCacheNewtonsoftJsonSerializer
+	: IFusionCacheSerializer
 {
 	/// <summary>
-	/// An implementation of <see cref="IFusionCacheSerializer"/> which uses the Newtonsoft Json.NET serializer.
+	/// Create a new instance of a <see cref="FusionCacheNewtonsoftJsonSerializer"/> object.
 	/// </summary>
-	public class FusionCacheNewtonsoftJsonSerializer
-		: IFusionCacheSerializer
+	/// <param name="settings">The optional <see cref="JsonSerializerSettings"/> object to use.</param>
+	public FusionCacheNewtonsoftJsonSerializer(JsonSerializerSettings? settings = null)
 	{
-		/// <summary>
-		/// Create a new instance of a <see cref="FusionCacheNewtonsoftJsonSerializer"/> object.
-		/// </summary>
-		/// <param name="settings">The optional <see cref="JsonSerializerSettings"/> object to use.</param>
-		public FusionCacheNewtonsoftJsonSerializer(JsonSerializerSettings? settings = null)
-		{
-			Settings = settings;
-		}
+		Settings = settings;
+	}
 
-		private readonly JsonSerializerSettings? Settings;
+	private readonly JsonSerializerSettings? Settings;
 
-		/// <inheritdoc />
-		public byte[] Serialize<T>(T? obj)
-		{
+	/// <inheritdoc />
+	public byte[] Serialize<T>(T? obj)
+	{
 #pragma warning disable CS8604 // Possible null reference argument.
-			return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, Settings));
+		return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, Settings));
 #pragma warning restore CS8604 // Possible null reference argument.
-		}
+	}
 
-		/// <inheritdoc />
-		public T? Deserialize<T>(byte[] data)
-		{
-			return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data), Settings);
-		}
+	/// <inheritdoc />
+	public T? Deserialize<T>(byte[] data)
+	{
+		return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data), Settings);
+	}
 
-		/// <inheritdoc />
-		public ValueTask<byte[]> SerializeAsync<T>(T? obj)
-		{
-			// TODO: DO BETTER HERE
-			return new ValueTask<byte[]>(Serialize<T>(obj));
-		}
+	/// <inheritdoc />
+	public ValueTask<byte[]> SerializeAsync<T>(T? obj)
+	{
+		// TODO: DO BETTER HERE
+		return new ValueTask<byte[]>(Serialize<T>(obj));
+	}
 
-		/// <inheritdoc />
-		public ValueTask<T?> DeserializeAsync<T>(byte[] data)
-		{
-			// TODO: DO BETTER HERE
-			return new ValueTask<T?>(Deserialize<T>(data));
-		}
+	/// <inheritdoc />
+	public ValueTask<T?> DeserializeAsync<T>(byte[] data)
+	{
+		// TODO: DO BETTER HERE
+		return new ValueTask<T?>(Deserialize<T>(data));
 	}
 }
--- src/ZiggyCreatures.FusionCache.Serialization.NewtonsoftJson/FusionCacheNewtonsoftJsonSerializerServiceCollectionExtensions.cs
+++ src/ZiggyCreatures.FusionCache.Serialization.NewtonsoftJson/FusionCacheNewtonsoftJsonSerializerServiceCollectionExtensions.cs
@@ -4,27 +4,26 @@ using Newtonsoft.Json;
 using ZiggyCreatures.Caching.Fusion.Serialization;
 using ZiggyCreatures.Caching.Fusion.Serialization.NewtonsoftJson;
 
-namespace Microsoft.Extensions.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for setting up FusionCache related services in an <see cref="IServiceCollection" />.
+/// </summary>
+public static class FusionCacheNewtonsoftJsonSerializerServiceCollectionExtensions
 {
 	/// <summary>
-	/// Extension methods for setting up FusionCache related services in an <see cref="IServiceCollection" />.
+	/// Adds an implementation of <see cref="IFusionCacheSerializer"/> based on the Newtonsoft JSON.NET one.
 	/// </summary>
-	public static class FusionCacheNewtonsoftJsonSerializerServiceCollectionExtensions
+	/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+	/// <param name="settings">The <see cref="JsonSerializerSettings"/> to use.</param>
+	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+	public static IServiceCollection AddFusionCacheNewtonsoftJsonSerializer(this IServiceCollection services, JsonSerializerSettings? settings = null)
 	{
-		/// <summary>
-		/// Adds an implementation of <see cref="IFusionCacheSerializer"/> based on the Newtonsoft JSON.NET one.
-		/// </summary>
-		/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
-		/// <param name="settings">The <see cref="JsonSerializerSettings"/> to use.</param>
-		/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-		public static IServiceCollection AddFusionCacheNewtonsoftJsonSerializer(this IServiceCollection services, JsonSerializerSettings? settings = null)
-		{
-			if (services is null)
-				throw new ArgumentNullException(nameof(services));
+		if (services is null)
+			throw new ArgumentNullException(nameof(services));
 
-			services.TryAddSingleton<IFusionCacheSerializer>(_ => new FusionCacheNewtonsoftJsonSerializer(settings));
+		services.TryAddSingleton<IFusionCacheSerializer>(_ => new FusionCacheNewtonsoftJsonSerializer(settings));
 
-			return services;
-		}
+		return services;
 	}
 }
