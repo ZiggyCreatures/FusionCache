66f614c4b31d49c312989f0f4e8002b8a5247cd4
Jody Donetti <indastria@gmail.com>
2022-12-07 19:10:24 +0100

    Add missing service collection extensions to Cysharp MemoryPack serializer

---
+++ src/ZiggyCreatures.FusionCache.Serialization.CysharpMemoryPack/FusionCacheCysharpMemoryPackSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MemoryPack;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ZiggyCreatures.Caching.Fusion.Serialization;
+using ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for setting up FusionCache related services in an <see cref="IServiceCollection" />.
+/// </summary>
+public static class FusionCacheCysharpMemoryPackSerializerServiceCollectionExtensions
+{
+	/// <summary>
+	/// Adds an implementation of <see cref="IFusionCacheSerializer"/> which uses Cysharp's MemoryPack serializer.
+	/// </summary>
+	/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+	/// <param name="options">The <see cref="MemoryPackSerializerOptions"/> to use.</param>
+	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+	public static IServiceCollection AddFusionCacheCysharpMemoryPackSerializer(this IServiceCollection services, MemoryPackSerializerOptions? options = null)
+	{
+		if (services is null)
+			throw new ArgumentNullException(nameof(services));
+
+		services.TryAddSingleton<IFusionCacheSerializer>(_ => new FusionCacheCysharpMemoryPackSerializer(options));
+
+		return services;
+	}
+}
