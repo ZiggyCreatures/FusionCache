30eaf275673d05006074ee9335654f2806b7f2e9
Jody Donetti <indastria@gmail.com>
2022-12-11 15:29:28 +0100

    Add multi targeting (std 2.1 + 7) + switch to ext formatters

---
+++ src/ZiggyCreatures.FusionCache.Serialization.CysharpMemoryPack/Internals/SerializableFusionCacheDistributedEntry.cs
@@ -0,0 +1,61 @@
+﻿using MemoryPack;
+using ZiggyCreatures.Caching.Fusion.Internals;
+using ZiggyCreatures.Caching.Fusion.Internals.Distributed;
+
+namespace ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack.Internals
+{
+	[MemoryPackable]
+	internal partial class SerializableFusionCacheDistributedEntry<TValue>
+	{
+		[MemoryPackIgnore]
+		public readonly FusionCacheDistributedEntry<TValue>? Entry;
+
+		[MemoryPackInclude]
+		public bool IsNull => Entry is null;
+
+		[MemoryPackInclude]
+		public TValue? Value => Entry is not null ? Entry.Value : default;
+
+		[MemoryPackAllowSerialize]
+		public FusionCacheEntryMetadata? Metadata => Entry?.Metadata;
+
+		[MemoryPackConstructor]
+		SerializableFusionCacheDistributedEntry(bool isNull, TValue value, FusionCacheEntryMetadata? metadata)
+		{
+			if (isNull)
+			{
+				this.Entry = null;
+			}
+			else
+			{
+				this.Entry = new FusionCacheDistributedEntry<TValue>(value, metadata);
+			}
+		}
+
+		public SerializableFusionCacheDistributedEntry(FusionCacheDistributedEntry<TValue>? entry)
+		{
+			this.Entry = entry;
+		}
+	}
+
+	internal class FusionCacheDistributedEntryFormatter<TValue> : MemoryPackFormatter<FusionCacheDistributedEntry<TValue>>
+	{
+		public override void Serialize<TBufferWriter>(ref MemoryPackWriter<TBufferWriter> writer, scoped ref FusionCacheDistributedEntry<TValue>? value)
+		{
+			writer.WritePackable(new SerializableFusionCacheDistributedEntry<TValue>(value));
+		}
+
+		public override void Deserialize(ref MemoryPackReader reader, scoped ref FusionCacheDistributedEntry<TValue>? value)
+		{
+			var wrapped = reader.ReadPackable<SerializableFusionCacheDistributedEntry<TValue>>();
+			if (wrapped is not null)
+			{
+				value = wrapped.Entry;
+			}
+			else
+			{
+				value = null;
+			}
+		}
+	}
+}
---
+++ src/ZiggyCreatures.FusionCache.Serialization.CysharpMemoryPack/Internals/SerializableFusionCacheEntryMetadata.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using MemoryPack;
+using ZiggyCreatures.Caching.Fusion.Internals;
+
+namespace ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack.Internals
+{
+	[MemoryPackable]
+	internal partial class SerializableFusionCacheEntryMetadata
+	{
+		[MemoryPackIgnore]
+		public readonly FusionCacheEntryMetadata? Metadata;
+
+		[MemoryPackInclude]
+		public bool IsNull => Metadata is null;
+
+		[MemoryPackInclude]
+		public DateTimeOffset LogicalExpiration => (Metadata?.LogicalExpiration) ?? default;
+
+		[MemoryPackInclude]
+		public bool IsFromFailSafe => (Metadata?.IsFromFailSafe) ?? default;
+
+		[MemoryPackConstructor]
+		SerializableFusionCacheEntryMetadata(bool isNull, DateTimeOffset logicalExpiration, bool isFromFailSafe)
+		{
+			if (isNull)
+			{
+				this.Metadata = null;
+			}
+			else
+			{
+				this.Metadata = new FusionCacheEntryMetadata(logicalExpiration, isFromFailSafe);
+			}
+		}
+
+		public SerializableFusionCacheEntryMetadata(FusionCacheEntryMetadata? metadata)
+		{
+			this.Metadata = metadata;
+		}
+	}
+
+	internal class FusionCacheEntryMetadataFormatter : MemoryPackFormatter<FusionCacheEntryMetadata>
+	{
+		public override void Serialize<TBufferWriter>(ref MemoryPackWriter<TBufferWriter> writer, scoped ref FusionCacheEntryMetadata? value)
+		{
+			writer.WritePackable(new SerializableFusionCacheEntryMetadata(value));
+		}
+
+		public override void Deserialize(ref MemoryPackReader reader, scoped ref FusionCacheEntryMetadata? value)
+		{
+			var wrapped = reader.ReadPackable<SerializableFusionCacheEntryMetadata>();
+			if (wrapped is not null)
+			{
+				value = wrapped.Metadata;
+			}
+			else
+			{
+				value = null;
+			}
+		}
+	}
+}
