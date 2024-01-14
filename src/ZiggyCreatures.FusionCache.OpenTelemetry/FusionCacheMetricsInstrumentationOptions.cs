50179da341197c240f33a15aeca8cdc9a4c5504d
Jody Donetti <indastria@gmail.com>
2024-01-14 19:13:02 +0100

    Add OpenTelemetry instrumentation project

---
+++ src/ZiggyCreatures.FusionCache.OpenTelemetry/FusionCacheMetricsInstrumentationOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace ZiggyCreatures.FusionCache.OpenTelemetry
+{
+	/// <summary>
+	/// Represents the options available for the metrics instrumentation of FusionCache.
+	/// </summary>
+	public class FusionCacheMetricsInstrumentationOptions
+	{
+		/// <summary>
+		/// Include metrics for the memory level. (default: <see langword="false"/>)
+		/// </summary>
+		public bool IncludeMemoryLevel { get; set; } = false;
+
+		/// <summary>
+		/// Include metrics for the distributed level. (default: <see langword="false"/>)
+		/// </summary>
+		public bool IncludeDistributedLevel { get; set; } = false;
+
+		/// <summary>
+		/// Include metrics for the backplane. (default: <see langword="false"/>)
+		/// </summary>
+		public bool IncludeBackplane { get; set; } = false;
+	}
+}
---
+++ src/ZiggyCreatures.FusionCache.OpenTelemetry/FusionCacheTracesInstrumentationOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace ZiggyCreatures.FusionCache.OpenTelemetry
+{
+	/// <summary>
+	/// Represents the options available for the traces instrumentation of FusionCache.
+	/// </summary>
+	public class FusionCacheTracesInstrumentationOptions
+	{
+		/// <summary>
+		/// Include traces for the memory level. (default: <see langword="false"/>)
+		/// </summary>
+		public bool IncludeMemoryLevel { get; set; } = false;
+
+		/// <summary>
+		/// Include traces for the distributed level. (default: <see langword="true"/>)
+		/// </summary>
+		public bool IncludeDistributedLevel { get; set; } = true;
+
+		/// <summary>
+		/// Include traces for the backplane. (default: <see langword="true"/>)
+		/// </summary>
+		public bool IncludeBackplane { get; set; } = true;
+	}
+}
