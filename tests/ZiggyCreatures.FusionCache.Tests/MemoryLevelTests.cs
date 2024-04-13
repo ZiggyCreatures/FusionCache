f0cd9c71c3a104908dea09de3ff0ab610ea55927
Jody Donetti <indastria@gmail.com>
2024-04-13 10:02:28 +0200

    Add missing NullFusionCache sync test

--- tests/ZiggyCreatures.FusionCache.Tests/MemoryLevelTests.cs
+++ tests/ZiggyCreatures.FusionCache.Tests/MemoryLevelTests.cs
@@ -1245,6 +1245,144 @@ public class MemoryLevelTests
 	}
 
 	[Fact]
+	public async Task CanHandleEagerRefreshCancellationAsync()
+	{
+		var duration = TimeSpan.FromSeconds(2);
+		var lockTimeout = TimeSpan.FromSeconds(10);
+		var eagerRefreshThreshold = 0.1f;
+		var eagerRefreshDelay = TimeSpan.FromSeconds(5);
+
+		using var cache = new FusionCache(new FusionCacheOptions(), logger: CreateXUnitLogger<FusionCache>());
+
+		cache.DefaultEntryOptions.Duration = duration;
+		cache.DefaultEntryOptions.EagerRefreshThreshold = eagerRefreshThreshold;
+
+		// EXECUTE FACTORY
+		var v1 = await cache.GetOrSetAsync<long>("foo", async _ => DateTimeOffset.UtcNow.Ticks);
+
+		// USE CACHED VALUE
+		var v2 = await cache.GetOrSetAsync<long>("foo", async _ => DateTimeOffset.UtcNow.Ticks);
+
+		// WAIT FOR EAGER REFRESH THRESHOLD TO BE HIT
+		var eagerDuration = TimeSpan.FromMilliseconds(duration.TotalMilliseconds * eagerRefreshThreshold).Add(TimeSpan.FromMilliseconds(10));
+		await Task.Delay(eagerDuration);
+
+		// EAGER REFRESH KICKS IN
+		var eagerRefreshIsStarted = false;
+		var eagerRefreshIsEnded = false;
+		using var cts = new CancellationTokenSource();
+		var v3 = await cache.GetOrSetAsync<long>(
+			"foo",
+			async ct =>
+			{
+				eagerRefreshIsStarted = true;
+
+				await Task.Delay(eagerRefreshDelay);
+
+				ct.ThrowIfCancellationRequested();
+
+				eagerRefreshIsEnded = true;
+
+				return DateTimeOffset.UtcNow.Ticks;
+			},
+			token: cts.Token
+		);
+
+		// ALLOW EAGER REFRESH TO START
+		await Task.Delay(TimeSpan.FromMilliseconds(50));
+
+		// CANCEL
+		cts.Cancel();
+
+		// WAIT FOR THE BACKGROUND FACTORY (EAGER REFRESH) TO COMPLETE
+		await Task.Delay(eagerRefreshDelay.PlusALittleBit());
+
+		// GET THE REFRESHED VALUE
+		var sw = Stopwatch.StartNew();
+		var v4 = await cache.GetOrSetAsync<long>("foo", async _ => DateTimeOffset.UtcNow.Ticks, options =>
+		{
+			options.LockTimeout = lockTimeout;
+		});
+		sw.Stop();
+
+		Assert.Equal(v1, v2);
+		Assert.Equal(v2, v3);
+		Assert.True(eagerRefreshIsStarted);
+		Assert.False(eagerRefreshIsEnded);
+		Assert.True(sw.Elapsed < lockTimeout);
+		Assert.True(v4 > v3);
+	}
+
+	[Fact]
+	public void CanHandleEagerRefreshCancellation()
+	{
+		var duration = TimeSpan.FromSeconds(2);
+		var lockTimeout = TimeSpan.FromSeconds(10);
+		var eagerRefreshThreshold = 0.1f;
+		var eagerRefreshDelay = TimeSpan.FromSeconds(5);
+
+		using var cache = new FusionCache(new FusionCacheOptions(), logger: CreateXUnitLogger<FusionCache>());
+
+		cache.DefaultEntryOptions.Duration = duration;
+		cache.DefaultEntryOptions.EagerRefreshThreshold = eagerRefreshThreshold;
+
+		// EXECUTE FACTORY
+		var v1 = cache.GetOrSet<long>("foo", _ => DateTimeOffset.UtcNow.Ticks);
+
+		// USE CACHED VALUE
+		var v2 = cache.GetOrSet<long>("foo", _ => DateTimeOffset.UtcNow.Ticks);
+
+		// WAIT FOR EAGER REFRESH THRESHOLD TO BE HIT
+		var eagerDuration = TimeSpan.FromMilliseconds(duration.TotalMilliseconds * eagerRefreshThreshold).Add(TimeSpan.FromMilliseconds(10));
+		Thread.Sleep(eagerDuration);
+
+		// EAGER REFRESH KICKS IN
+		var eagerRefreshIsStarted = false;
+		var eagerRefreshIsEnded = false;
+		using var cts = new CancellationTokenSource();
+		var v3 = cache.GetOrSet<long>(
+			"foo",
+			ct =>
+			{
+				eagerRefreshIsStarted = true;
+
+				Thread.Sleep(eagerRefreshDelay);
+
+				ct.ThrowIfCancellationRequested();
+
+				eagerRefreshIsEnded = true;
+
+				return DateTimeOffset.UtcNow.Ticks;
+			},
+			token: cts.Token
+		);
+
+		// ALLOW EAGER REFRESH TO START
+		Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+		// CANCEL
+		cts.Cancel();
+
+		// WAIT FOR THE BACKGROUND FACTORY (EAGER REFRESH) TO COMPLETE
+		Thread.Sleep(eagerRefreshDelay.PlusALittleBit());
+
+		// GET THE REFRESHED VALUE
+		var sw = Stopwatch.StartNew();
+		var v4 = cache.GetOrSet<long>("foo", _ => DateTimeOffset.UtcNow.Ticks, options =>
+		{
+			options.LockTimeout = lockTimeout;
+		});
+		sw.Stop();
+
+		Assert.Equal(v1, v2);
+		Assert.Equal(v2, v3);
+		Assert.True(eagerRefreshIsStarted);
+		Assert.False(eagerRefreshIsEnded);
+		Assert.True(sw.Elapsed < lockTimeout);
+		Assert.True(v4 > v3);
+	}
+
+	[Fact]
 	public async Task NormalFactoryExecutionWaitsForInFlightEagerRefreshAsync()
 	{
 		var duration = TimeSpan.FromSeconds(2);
@@ -1518,6 +1656,57 @@ public class MemoryLevelTests
 		await Assert.ThrowsAsync<UnreachableException>(async () =>
 		{
 			_ = await cache.GetOrSetAsync<int>("qux", async _ => throw new UnreachableException("Sloths"));
+		});
+	}
+
+	[Fact]
+	public void CanUseNullFusionCache()
+	{
+		using var cache = new NullFusionCache(new FusionCacheOptions()
+		{
+			CacheName = "SlothsAreCool42",
+			DefaultEntryOptions = new FusionCacheEntryOptions()
+			{
+				IsFailSafeEnabled = true,
+				Duration = TimeSpan.FromMinutes(123)
+			}
+		});
+
+		cache.Set<int>("foo", 42);
+
+		var maybeFoo1 = cache.TryGet<int>("foo");
+
+		cache.Remove("foo");
+
+		var maybeBar1 = cache.TryGet<int>("bar");
+
+		cache.Expire("qux");
+
+		var qux1 = cache.GetOrSet("qux", _ => 1);
+		var qux2 = cache.GetOrSet("qux", _ => 2);
+		var qux3 = cache.GetOrSet("qux", _ => 3);
+		var qux4 = cache.GetOrDefault("qux", 4);
+
+		Assert.Equal("SlothsAreCool42", cache.CacheName);
+		Assert.False(string.IsNullOrWhiteSpace(cache.InstanceId));
+
+		Assert.False(cache.HasDistributedCache);
+		Assert.False(cache.HasBackplane);
+
+		Assert.True(cache.DefaultEntryOptions.IsFailSafeEnabled);
+		Assert.Equal(TimeSpan.FromMinutes(123), cache.DefaultEntryOptions.Duration);
+
+		Assert.False(maybeFoo1.HasValue);
+		Assert.False(maybeBar1.HasValue);
+
+		Assert.Equal(1, qux1);
+		Assert.Equal(2, qux2);
+		Assert.Equal(3, qux3);
+		Assert.Equal(4, qux4);
+
+		Assert.Throws<UnreachableException>(() =>
+		{
+			_ = cache.GetOrSet<int>("qux", _ => throw new UnreachableException("Sloths"));
 		});
 	}
 }
