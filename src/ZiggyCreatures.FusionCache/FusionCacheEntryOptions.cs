3be88de4d0e808f78a5e553077e93736d60faf5d
Jody Donetti <indastria@gmail.com>
2024-04-24 00:12:10 +0200

    Revert "#230 add in TFM's so dependencies can be optimised (#231)"
    This reverts commit 85d599b1b7e2b589bcfc1ff7dd4473399a147486.

--- src/ZiggyCreatures.FusionCache/FusionCacheEntryOptions.cs
+++ src/ZiggyCreatures.FusionCache/FusionCacheEntryOptions.cs
@@ -685,25 +685,22 @@ public sealed class FusionCacheEntryOptions
 		// PHYSICAL DURATION
 		TimeSpan physicalDuration;
 		TimeSpan durationToUse;
+		TimeSpan failSafeMaxDurationToUse;
+		bool incoherentFailSafeMaxDuration = false;
 
 		durationToUse = DistributedCacheDuration ?? Duration;
 
 		if (IsFailSafeEnabled == false)
 		{
-			// FAIL-SAFE DISABLED
 			physicalDuration = durationToUse;
 		}
 		else
 		{
-			// FAIL-SAFE ENABLED
-			var failSafeMaxDurationToUse = DistributedCacheFailSafeMaxDuration ?? FailSafeMaxDuration;
+			failSafeMaxDurationToUse = DistributedCacheFailSafeMaxDuration ?? FailSafeMaxDuration;
 			if (failSafeMaxDurationToUse < durationToUse)
 			{
-				// INCOHERENT DURATION
+				incoherentFailSafeMaxDuration = true;
 				physicalDuration = durationToUse;
-
-				if (logger?.IsEnabled(options.IncoherentOptionsNormalizationLogLevel) ?? false)
-					logger.Log(options.IncoherentOptionsNormalizationLogLevel, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): DistributedCacheFailSafeMaxDuration/FailSafeMaxDuration {FailSafeMaxDuration} was lower than the DistributedCacheDuration/Duration {Duration} on {Options} {MemoryOptions}. Duration has been used instead.", options.CacheName, options.InstanceId, operationId, key, failSafeMaxDurationToUse.ToLogString(), durationToUse.ToLogString(), this.ToLogString(), res.ToLogString());
 			}
 			else
 			{
@@ -712,6 +709,13 @@ public sealed class FusionCacheEntryOptions
 		}
 
 		res.AbsoluteExpiration = FusionCacheInternalUtils.GetNormalizedAbsoluteExpiration(physicalDuration, this, false);
+
+		// INCOHERENT DURATION
+		if (incoherentFailSafeMaxDuration)
+		{
+			if (logger?.IsEnabled(options.IncoherentOptionsNormalizationLogLevel) ?? false)
+				logger.Log(options.IncoherentOptionsNormalizationLogLevel, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): DistributedCacheFailSafeMaxDuration/FailSafeMaxDuration {FailSafeMaxDuration} was lower than the DistributedCacheDuration/Duration {Duration} on {Options} {MemoryOptions}. Duration has been used instead.", options.CacheName, options.InstanceId, operationId, key, failSafeMaxDurationToUse.ToLogString(), durationToUse.ToLogString(), this.ToLogString(), res.ToLogString());
+		}
 
 		return res;
 	}
