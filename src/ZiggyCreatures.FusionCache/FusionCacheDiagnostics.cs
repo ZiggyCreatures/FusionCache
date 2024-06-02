08f769b146cdef8823ce689995affd2a6e12a080
Jody Donetti <indastria@gmail.com>
2024-06-02 21:37:21 +0200

    v1.2.0

--- src/ZiggyCreatures.FusionCache/FusionCacheDiagnostics.cs
+++ src/ZiggyCreatures.FusionCache/FusionCacheDiagnostics.cs
@@ -8,7 +8,7 @@
 		/// <summary>
 		/// The current version of FusionCache.
 		/// </summary>
-		public const string FusionCacheVersion = "1.2.0-preview1";
+		public const string FusionCacheVersion = "1.2.0";
 
 		/// <summary>
 		/// The activity source name for FusionCache.
