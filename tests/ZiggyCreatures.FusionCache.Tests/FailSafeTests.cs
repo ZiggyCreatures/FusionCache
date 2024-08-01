855722d0374b55330b14c3bd0e0948d655017ed2
Carl Meyertons <carlmeyertons@gmail.com>
2024-08-01 16:11:48 -0500

    Adaptive Options Should Be Evaluated On Error (#276)
    * add specific fail-safe tests
    * update adaptive options on exception
    
    ---------
    
    Co-authored-by: Jody Donetti

---
+++ tests/ZiggyCreatures.FusionCache.Tests/FailSafeTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using ZiggyCreatures.Caching.Fusion;
+
+namespace FusionCacheTests;
+
+public class FailSafeTests
+{
+	private readonly FusionCache _cache;
+	private readonly string _key;
+
+	public FailSafeTests()
+	{
+		var options = new FusionCacheOptions();
+
+		options.DefaultEntryOptions.Duration = TimeSpan.FromMinutes(5);
+		options.DefaultEntryOptions.IsFailSafeEnabled = true; // enable by default
+
+		_cache = new FusionCache(options);
+		_key = "foo";
+
+		_cache.GetOrSet(_key, "bar"); // init the key
+
+		_cache.Expire(_key); // logically expire the key so the fail safe logic triggers.
+	}
+
+	[Fact]
+	public async Task FailSafe_CanBeDisabled_OnFactoryFailureAsync()
+	{
+		await Assert.ThrowsAsync<Exception>(async () =>
+		{
+			await _cache.GetOrSetAsync<string>(_key, (ctx, ct) =>
+			{
+				try
+				{
+					throw new Exception("Factory failed");
+				}
+				finally
+				{
+					ctx.Options.SetFailSafe(false); // disable fail safe.
+				}
+			});
+		});
+	}
+
+	[Fact]
+	public async Task FailSafe_CanBeDisabled_OnFactoryFailure()
+	{
+		Assert.Throws<Exception>(() =>
+		{
+			_cache.GetOrSet<string>(_key, (ctx, ct) =>
+			{
+				try
+				{
+					throw new Exception("Factory failed");
+				}
+				finally
+				{
+					ctx.Options.SetFailSafe(false); // disable fail safe.
+				}
+			});
+		});
+	}
+}
+
