bd5bd73a850f244088b978f6f70b53436c659018
Jody Donetti <indastria@gmail.com>
2024-08-11 17:43:12 +0200

    Better FusionCacheProvider: direct caches handling, better logging, etc

--- src/ZiggyCreatures.FusionCache/Internals/Provider/FusionCacheProvider.cs
+++ src/ZiggyCreatures.FusionCache/Internals/Provider/FusionCacheProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Frozen;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace ZiggyCreatures.Caching.Fusion.Internals.Provider;
 
@@ -9,33 +10,62 @@ internal sealed class FusionCacheProvider
 	: IFusionCacheProvider
 {
 	private readonly FrozenDictionary<string, LazyNamedCache?> _caches;
+	private readonly ILogger<FusionCache>? _logger;
 
-	public FusionCacheProvider(IEnumerable<IFusionCache> directCaches, IEnumerable<LazyNamedCache> lazyNamedCaches)
+	public FusionCacheProvider(IEnumerable<IFusionCache> directCaches, IEnumerable<LazyNamedCache> lazyNamedCaches, ILogger<FusionCache>? logger = null)
 	{
-		List<KeyValuePair<string, LazyNamedCache?>> caches = [];
+		_logger = logger;
+
+		List<KeyValuePair<string, LazyNamedCache?>> items = [];
 		foreach (var group in lazyNamedCaches.GroupBy(g => g.CacheName))
 		{
 			if (group.Count() == 1)
 			{
 				// ONLY 1 CACHE -> ADD IT
-				caches.Add(new(group.Key, group.First()));
+				items.Add(new(group.Key, group.First()));
 			}
 			else
 			{
 				// MORE THAN 1 CACHE -> ADD NULL
 				// NOTE: THIS WILL SIGNAL THAT THERE WERE MULTIPLE ONES AND, SINCE
 				// THEY WILL NOT BE ACCESSIBLE ANYWAY, WILL SAVE SOME MEMORY
-				caches.Add(new(group.Key, null));
+				if (_logger?.IsEnabled(LogLevel.Warning) ?? false)
+					_logger.Log(LogLevel.Warning, "FUSION: multiple FusionCache registrations detected with cache name {CacheName}. This should be avoided as it will lead to surprises.", group.Key);
+
+				items.Add(new(group.Key, null));
 			}
 		}
 
-		var defaultCache = directCaches.LastOrDefault();
-		if (defaultCache is not null)
+		var directCachesCount = directCaches.Count();
+		if (directCachesCount > 0)
 		{
-			caches.Add(new(defaultCache.CacheName, new LazyNamedCache(defaultCache.CacheName, defaultCache)));
+			var nonDefaultDirectCacheNames = directCaches.Where(x => x.CacheName != FusionCacheOptions.DefaultCacheName).Select(x => x.CacheName).Distinct().ToArray();
+			if (nonDefaultDirectCacheNames.Length > 0)
+			{
+				if (_logger?.IsEnabled(LogLevel.Warning) ?? false)
+					_logger.Log(LogLevel.Warning, "FUSION: one or more direct IFusionCache registrations have been detected with a cache name which is not the default one: {CacheNames}.", string.Join(", ", nonDefaultDirectCacheNames));
+			}
+
+			if (directCachesCount > 1)
+			{
+				if (_logger?.IsEnabled(LogLevel.Warning) ?? false)
+					_logger.Log(LogLevel.Warning, "FUSION: multiple direct IFusionCache registrations have been detected. The last one will be used, as per Microsoft standard DI implementation, but it should be avoided when possible to prevent bad surprises.");
+			}
+
+			// THE LAST ONE REGISTERED WILL BE THE ONE USED, FOLLOWING
+			// THE STANDARD BEHAVIOR OF MICROSOFT'S DI CONTAINER.
+			var directCache = directCaches.Last();
+
+			if (directCache.CacheName != FusionCacheOptions.DefaultCacheName)
+			{
+				if (_logger?.IsEnabled(LogLevel.Warning) ?? false)
+					_logger.Log(LogLevel.Warning, "FUSION: the direct IFusionCache registration that will be used has a cache name ({CacheName}) that is different from the default one. This should be avoided when possible to prevent bad surprises.", directCache.CacheName);
+			}
+
+			items.Add(new(directCache.CacheName, new LazyNamedCache(directCache.CacheName, directCache)));
 		}
 
-		_caches = caches.ToFrozenDictionary();
+		_caches = items.ToFrozenDictionary();
 	}
 
 	public IFusionCache? GetCacheOrNull(string cacheName)
