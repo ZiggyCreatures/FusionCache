3b0079dc4bce8d7300926664fde7640c2d221903
Jody Donetti <indastria@gmail.com>
2024-08-11 17:44:37 +0200

    Better tests

--- tests/ZiggyCreatures.FusionCache.Tests/Stuff/TestsUtils.cs
+++ tests/ZiggyCreatures.FusionCache.Tests/Stuff/TestsUtils.cs
@@ -1,7 +1,16 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 using ZiggyCreatures.Caching.Fusion;
+using ZiggyCreatures.Caching.Fusion.Backplane;
+using ZiggyCreatures.Caching.Fusion.Backplane.StackExchangeRedis;
+using ZiggyCreatures.Caching.Fusion.Internals.Backplane;
+using ZiggyCreatures.Caching.Fusion.Internals.Distributed;
+using ZiggyCreatures.Caching.Fusion.Locking;
+using ZiggyCreatures.Caching.Fusion.Plugins;
 using ZiggyCreatures.Caching.Fusion.Serialization;
 using ZiggyCreatures.Caching.Fusion.Serialization.CysharpMemoryPack;
 using ZiggyCreatures.Caching.Fusion.Serialization.NeueccMessagePack;
@@ -11,20 +20,6 @@ using ZiggyCreatures.Caching.Fusion.Serialization.ServiceStackJson;
 using ZiggyCreatures.Caching.Fusion.Serialization.SystemTextJson;
 
 namespace FusionCacheTests.Stuff;
-
-public enum SerializerType
-{
-	// JSON
-	NewtonsoftJson = 0,
-	SystemTextJson = 1,
-	ServiceStackJson = 2,
-	// MESSAGEPACK
-	NeueccMessagePack = 10,
-	// PROTOBUF
-	ProtoBufNet = 20,
-	// MEMORYPACK
-	CysharpMemoryPack = 30,
-}
 
 public static class TestsUtils
 {
@@ -77,20 +72,63 @@ public static class TestsUtils
 			options.AllowTimedOutFactoryBackgroundCompletion = keepTimedOutFactoryResult.Value;
 		return options;
 	}
-}
 
-public class SerializerTypesClassData : IEnumerable<object[]>
-{
-	public IEnumerator<object[]> GetEnumerator()
+	public static ILogger? GetLogger(IFusionCache cache)
 	{
-		foreach (var x in Enum.GetValues<SerializerType>())
-		{
-			yield return [x];
-		}
+		return typeof(FusionCache).GetField("_logger", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache) as ILogger;
 	}
 
-	IEnumerator IEnumerable.GetEnumerator()
+	public static IFusionCacheMemoryLocker? GetMemoryLocker(IFusionCache cache)
 	{
-		return GetEnumerator();
+		return typeof(FusionCache).GetField("_memoryLocker", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache) as IFusionCacheMemoryLocker;
+	}
+
+	public static IMemoryCache? GetMemoryCache(IFusionCache cache)
+	{
+		var _mca = typeof(FusionCache).GetField("_mca", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache);
+		return _mca!.GetType().GetField("_cache", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(_mca) as IMemoryCache;
+	}
+
+	public static IFusionCacheSerializer? GetSerializer(IFusionCache cache)
+	{
+		var dca = typeof(FusionCache).GetField("_dca", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache) as DistributedCacheAccessor;
+		if (dca is null)
+			return null;
+
+		return typeof(DistributedCacheAccessor).GetField("_serializer", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(dca) as IFusionCacheSerializer;
+	}
+
+	public static IDistributedCache? GetDistributedCache<TDistributedCache>(IFusionCache cache)
+		where TDistributedCache : class, IDistributedCache
+	{
+		var dca = typeof(FusionCache).GetField("_dca", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache) as DistributedCacheAccessor;
+		if (dca is null)
+			return null;
+
+		return typeof(DistributedCacheAccessor).GetField("_cache", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(dca) as TDistributedCache;
+	}
+
+	public static TBackplane? GetBackplane<TBackplane>(IFusionCache cache)
+		where TBackplane : class, IFusionCacheBackplane
+	{
+		var bpa = typeof(FusionCache).GetField("_bpa", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache) as BackplaneAccessor;
+		if (bpa is null)
+			return null;
+
+		return typeof(BackplaneAccessor).GetField("_backplane", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(bpa) as TBackplane;
+	}
+
+	public static RedisBackplaneOptions? GetRedisBackplaneOptions(IFusionCache cache)
+	{
+		var backplane = GetBackplane<RedisBackplane>(cache);
+		if (backplane is null)
+			return null;
+
+		return typeof(RedisBackplane).GetField("_options", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(backplane) as RedisBackplaneOptions; ;
+	}
+
+	public static IFusionCachePlugin[]? GetPlugins(IFusionCache cache)
+	{
+		return (typeof(FusionCache).GetField("_plugins", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(cache) as List<IFusionCachePlugin>)?.ToArray();
 	}
 }
