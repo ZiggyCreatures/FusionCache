e9c43107278c124547382161f97ed73dc056a19e
Jody Donetti <indastria@gmail.com>
2024-09-15 19:12:59 +0200

    v1.4.0

--- src/ZiggyCreatures.FusionCache/FusionCacheDiagnostics.cs
+++ src/ZiggyCreatures.FusionCache/FusionCacheDiagnostics.cs
@@ -1,54 +1,53 @@
-﻿namespace ZiggyCreatures.Caching.Fusion
+﻿namespace ZiggyCreatures.Caching.Fusion;
+
+/// <summary>
+/// A support class for FusionCache diagnostics.
+/// </summary>
+public static class FusionCacheDiagnostics
 {
 	/// <summary>
-	/// A support class for FusionCache diagnostics.
+	/// The current version of FusionCache.
 	/// </summary>
-	public static class FusionCacheDiagnostics
-	{
-		/// <summary>
-		/// The current version of FusionCache.
-		/// </summary>
-		public const string FusionCacheVersion = "1.3.0";
+	public const string FusionCacheVersion = "1.4.0";
 
-		/// <summary>
-		/// The activity source name for FusionCache.
-		/// </summary>
-		public const string ActivitySourceName = "ZiggyCreatures.Caching.Fusion";
+	/// <summary>
+	/// The activity source name for FusionCache.
+	/// </summary>
+	public const string ActivitySourceName = "ZiggyCreatures.Caching.Fusion";
 
-		/// <summary>
-		/// The activity source name for the FusionCache memory level.
-		/// </summary>
-		public const string ActivitySourceNameMemoryLevel = "ZiggyCreatures.Caching.Fusion.Memory";
+	/// <summary>
+	/// The activity source name for the FusionCache memory level.
+	/// </summary>
+	public const string ActivitySourceNameMemoryLevel = "ZiggyCreatures.Caching.Fusion.Memory";
 
-		/// <summary>
-		/// The activity source name for the FusionCache distributed level.
-		/// </summary>
-		public const string ActivitySourceNameDistributedLevel = "ZiggyCreatures.Caching.Fusion.Distributed";
+	/// <summary>
+	/// The activity source name for the FusionCache distributed level.
+	/// </summary>
+	public const string ActivitySourceNameDistributedLevel = "ZiggyCreatures.Caching.Fusion.Distributed";
 
-		/// <summary>
-		/// The activity source name for the FusionCache backplane.
-		/// </summary>
-		public const string ActivitySourceNameBackplane = "ZiggyCreatures.Caching.Fusion.Backplane";
+	/// <summary>
+	/// The activity source name for the FusionCache backplane.
+	/// </summary>
+	public const string ActivitySourceNameBackplane = "ZiggyCreatures.Caching.Fusion.Backplane";
 
 
-		/// <summary>
-		/// The meter name for FusionCache.
-		/// </summary>
-		public const string MeterName = "ZiggyCreatures.Caching.Fusion";
+	/// <summary>
+	/// The meter name for FusionCache.
+	/// </summary>
+	public const string MeterName = "ZiggyCreatures.Caching.Fusion";
 
-		/// <summary>
-		/// The meter name for the FusionCache memory level.
-		/// </summary>
-		public const string MeterNameMemoryLevel = "ZiggyCreatures.Caching.Fusion.Memory";
+	/// <summary>
+	/// The meter name for the FusionCache memory level.
+	/// </summary>
+	public const string MeterNameMemoryLevel = "ZiggyCreatures.Caching.Fusion.Memory";
 
-		/// <summary>
-		/// The meter name for the FusionCache distributed level.
-		/// </summary>
-		public const string MeterNameDistributedLevel = "ZiggyCreatures.Caching.Fusion.Distributed";
+	/// <summary>
+	/// The meter name for the FusionCache distributed level.
+	/// </summary>
+	public const string MeterNameDistributedLevel = "ZiggyCreatures.Caching.Fusion.Distributed";
 
-		/// <summary>
-		/// The meter name for the FusionCache backplane.
-		/// </summary>
-		public const string MeterNameBackplane = "ZiggyCreatures.Caching.Fusion.Backplane";
-	}
+	/// <summary>
+	/// The meter name for the FusionCache backplane.
+	/// </summary>
+	public const string MeterNameBackplane = "ZiggyCreatures.Caching.Fusion.Backplane";
 }
