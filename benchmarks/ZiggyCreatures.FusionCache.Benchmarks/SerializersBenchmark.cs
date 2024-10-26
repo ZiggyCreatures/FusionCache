4bdb4f9b77f85c27ce469cd035a503211006fea1
Jody Donetti <indastria@gmail.com>
2024-10-26 17:56:56 +0200

    Minor NRT stuff

--- benchmarks/ZiggyCreatures.FusionCache.Benchmarks/SerializersBenchmark.cs
+++ benchmarks/ZiggyCreatures.FusionCache.Benchmarks/SerializersBenchmark.cs
@@ -52,10 +52,10 @@ public abstract class AbstractSerializersBenchmark
 		}
 	}
 
-	protected IFusionCacheSerializer _Normal;
-	protected IFusionCacheSerializer _Recyclable;
+	protected IFusionCacheSerializer _Normal = null!;
+	protected IFusionCacheSerializer _Recyclable = null!;
 	protected List<SampleModel> _Models = new List<SampleModel>();
-	protected byte[] _Blob;
+	protected byte[] _Blob = null!;
 
 	[Params(1, 100, 1_000)]
 	public int Size;
