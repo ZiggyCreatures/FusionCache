a429ea852541fb6734bc31511c11e147d76e4931
Jody Donetti <indastria@gmail.com>
2024-12-09 23:14:06 +0100

    Xml comments

--- src/ZiggyCreatures.FusionCache.MicrosoftHybridCache/FusionHybridCacheServiceCollectionExtensions.cs
+++ src/ZiggyCreatures.FusionCache.MicrosoftHybridCache/FusionHybridCacheServiceCollectionExtensions.cs
@@ -9,6 +9,11 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// </summary>
 public static class FusionHybridCacheServiceCollectionExtensions
 {
+	/// <summary>
+	/// Register this FusionCache instance also as a <see cref="HybridCache"/> service, so that it can be used even when you need to depend on Microsoft's own hybrid cache abstraction.
+	/// </summary>
+	/// <param name="builder">The <see cref="IFusionCacheBuilder" /> to act upon.</param>
+	/// <returns></returns>
 	public static IFusionCacheBuilder AsHybridCache(this IFusionCacheBuilder builder)
 	{
 		builder.Services.AddSingleton<HybridCache>(sp =>
@@ -19,6 +24,16 @@ public static class FusionHybridCacheServiceCollectionExtensions
 		return builder;
 	}
 
+	/// <summary>
+	/// Register this FusionCache instance also as a keyed <see cref="HybridCache"/> service, so that it can be used even when you need to depend on Microsoft's own hybrid cache abstraction, and even with the [FromKeyedServices] attribute usage.
+	/// <br/><br/>
+	/// <strong>DOCS:</strong> <see href="https://github.com/ZiggyCreatures/FusionCache/blob/main/docs/DependencyInjection.md"/>
+	/// <br/><br/>
+	/// <strong>DOCS:</strong> <see href="https://learn.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-8.0#keyed-services"/>
+	/// </summary>
+	/// <param name="builder">The <see cref="IFusionCacheBuilder" /> to act upon.</param>
+	/// <param name="serviceKey">The keyed service key to use.</param>
+	/// <returns></returns>
 	public static IFusionCacheBuilder AsKeyedHybridCache(this IFusionCacheBuilder builder, object? serviceKey)
 	{
 		builder.Services.AddKeyedSingleton<HybridCache>(serviceKey, (sp, _) =>
@@ -29,6 +44,15 @@ public static class FusionHybridCacheServiceCollectionExtensions
 		return builder;
 	}
 
+	/// <summary>
+	/// Register this FusionCache instance also as a keyed <see cref="HybridCache"/> service (with the CacheName as the serviceKey), so that it can be used even when you need to depend on Microsoft's own hybrid cache abstraction, and even with the [FromKeyedServices] attribute usage.
+	/// <br/><br/>
+	/// <strong>DOCS:</strong> <see href="https://github.com/ZiggyCreatures/FusionCache/blob/main/docs/DependencyInjection.md"/>
+	/// <br/><br/>
+	/// <strong>DOCS:</strong> <see href="https://learn.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-8.0#keyed-services"/>
+	/// </summary>
+	/// <param name="builder">The <see cref="IFusionCacheBuilder" /> to act upon.</param>
+	/// <returns></returns>
 	public static IFusionCacheBuilder AsKeyedHybridCacheByCacheName(this IFusionCacheBuilder builder)
 	{
 		return builder.AsKeyedHybridCache(builder.CacheName);
