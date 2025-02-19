683931ffefda34a3a5318c939c5e3182d77fe990
Jody Donetti <indastria@gmail.com>
2025-02-19 21:25:01 +0100

    Minor cleanup

--- src/ZiggyCreatures.FusionCache/Locking/ExperimentalMemoryLocker.cs
+++ src/ZiggyCreatures.FusionCache/Locking/ExperimentalMemoryLocker.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace ZiggyCreatures.Caching.Fusion.Locking;
 
