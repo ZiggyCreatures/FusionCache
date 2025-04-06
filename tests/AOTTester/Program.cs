1d94d301e68bd0bbdeb79fd33f91bc2163cd16c1
Jody Donetti <indastria@gmail.com>
2025-04-06 16:04:34 +0200

    Add AOT tester app

---
+++ tests/AOTTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using ZiggyCreatures.Caching.Fusion;
+
+namespace AOTTester;
+
+internal class Program
+{
+	static async Task Main(string[] args)
+	{
+		var cache = new FusionCache(new FusionCacheOptions());
+
+		var value = await cache.GetOrSetAsync<int>("foo", async (ctx, ct) => 42);
+
+		Console.WriteLine($"VALUE: {value}");
+	}
+}
