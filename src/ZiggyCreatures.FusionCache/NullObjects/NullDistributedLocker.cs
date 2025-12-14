e51e492b099109e21f820b1ac985fc73e200e414
Jody Donetti <indastria@gmail.com>
2025-12-14 20:40:15 +0100

    Add null impl of distributed locker

---
+++ src/ZiggyCreatures.FusionCache/NullObjects/NullDistributedLocker.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using ZiggyCreatures.Caching.Fusion.Locking;
+
+namespace ZiggyCreatures.Caching.Fusion.NullObjects;
+
+/// <summary>
+/// An implementation of <see cref="IFusionCacheDistributedLocker"/> that implements the null object pattern, meaning that it does nothing. Consider this a kind of a pass-through implementation.
+/// </summary>
+public class NullDistributedLocker
+	: IFusionCacheDistributedLocker
+{
+	/// <inheritdoc/>
+	public object? AcquireLock(string cacheName, string cacheInstanceId, string operationId, string key, TimeSpan timeout, ILogger? logger, CancellationToken token)
+	{
+		return null;
+	}
+
+	/// <inheritdoc/>
+	public ValueTask<object?> AcquireLockAsync(string cacheName, string cacheInstanceId, string operationId, string key, TimeSpan timeout, ILogger? logger, CancellationToken token)
+	{
+		return new ValueTask<object?>((object?)null);
+	}
+
+	/// <inheritdoc/>
+	public void ReleaseLock(string cacheName, string cacheInstanceId, string operationId, string key, object? lockObj, ILogger? logger, CancellationToken token)
+	{
+		// EMPTY
+	}
+
+	/// <inheritdoc/>
+	public ValueTask ReleaseLockAsync(string cacheName, string cacheInstanceId, string operationId, string key, object? lockObj, ILogger? logger, CancellationToken token)
+	{
+		return new ValueTask();
+	}
+
+	/// <inheritdoc/>
+	public void Dispose()
+	{
+		// EMPTY
+	}
+}
