d7e33398243ef6acd1a5ab5a3cbe5af738821197
Dmitry Tsarevich <dimhotepus@gmail.com>
2026-08-29 03:36:32 +0300

    Propagate client-side cancellations to the caller
    Fixes logs spam due to OperationCancelledException writes,
    Fixes propagation of client-side cancellations in ex web API requests.

--- src/ZiggyCreatures.FusionCache/Internals/Distributed/DistributedCacheAccessor_Async.cs
+++ src/ZiggyCreatures.FusionCache/Internals/Distributed/DistributedCacheAccessor_Async.cs
@@ -21,6 +21,13 @@ internal partial class DistributedCacheAccessor
 			if (_logger?.IsEnabled(LogLevel.Trace) ?? false)
 				_logger.Log(LogLevel.Trace, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] after " + actionDescription, _options.CacheName, _options.InstanceId, operationId, key);
 		}
+		catch (OperationCanceledException exc) when (token.IsCancellationRequested)
+		{
+			if (_logger?.IsEnabled(LogLevel.Debug) ?? false)
+				_logger.Log(LogLevel.Debug, exc, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] canceled " + actionDescription, _options.CacheName, _options.InstanceId, operationId, key);
+
+			throw;
+		}
 		catch (Exception exc)
 		{
 			ProcessError(operationId, key, exc, actionDescription);
@@ -84,6 +91,13 @@ internal partial class DistributedCacheAccessor
 			{
 				data = await _serializer.SerializeAsync(distributedEntry, token).ConfigureAwait(false);
 			}
+		}
+		catch (OperationCanceledException exc) when (token.IsCancellationRequested)
+		{
+			if (_logger?.IsEnabled(LogLevel.Debug) ?? false)
+				_logger.Log(LogLevel.Debug, exc, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] canceled serializing the entry {Entry}", _options.CacheName, _options.InstanceId, operationId, key, distributedEntry.ToLogString(_options.IncludeTagsInLogs));
+
+			throw;
 		}
 		catch (Exception exc)
 		{
@@ -165,6 +179,13 @@ internal partial class DistributedCacheAccessor
 				token: token
 			).ConfigureAwait(false);
 		}
+		catch (OperationCanceledException exc) when (token.IsCancellationRequested)
+		{
+			if (_logger?.IsEnabled(LogLevel.Debug) ?? false)
+				_logger.Log(LogLevel.Debug, exc, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] canceled get entry from distributed", _options.CacheName, _options.InstanceId, operationId, key);
+
+			throw;
+		}
 		catch (Exception exc)
 		{
 			ProcessError(operationId, key, exc, "getting entry from distributed");
@@ -244,6 +265,13 @@ internal partial class DistributedCacheAccessor
 			}
 
 			return (entry, isValid);
+		}
+		catch (OperationCanceledException exc) when (token.IsCancellationRequested)
+		{
+			if (_logger?.IsEnabled(LogLevel.Debug) ?? false)
+				_logger.Log(LogLevel.Debug, exc, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] cancelled while deserializing an entry", _options.CacheName, _options.InstanceId, operationId, key);
+
+			throw;
 		}
 		catch (Exception exc)
 		{
--- src/ZiggyCreatures.FusionCache/Internals/Distributed/DistributedCacheAccessor_Sync.cs
+++ src/ZiggyCreatures.FusionCache/Internals/Distributed/DistributedCacheAccessor_Sync.cs
@@ -21,6 +21,13 @@ internal partial class DistributedCacheAccessor
 			if (_logger?.IsEnabled(LogLevel.Trace) ?? false)
 				_logger.Log(LogLevel.Trace, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] after " + actionDescription, _options.CacheName, _options.InstanceId, operationId, key);
 		}
+		catch (OperationCanceledException exc) when (token.IsCancellationRequested)
+		{
+			if (_logger?.IsEnabled(LogLevel.Debug) ?? false)
+				_logger.Log(LogLevel.Debug, exc, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] canceled " + actionDescription, _options.CacheName, _options.InstanceId, operationId, key);
+			
+			throw;
+		}
 		catch (Exception exc)
 		{
 			ProcessError(operationId, key, exc, actionDescription);
@@ -157,6 +164,13 @@ internal partial class DistributedCacheAccessor
 				true,
 				token: token
 			);
+		}
+		catch (OperationCanceledException exc) when (token.IsCancellationRequested)
+		{
+			if (_logger?.IsEnabled(LogLevel.Debug) ?? false)
+				_logger.Log(LogLevel.Debug, exc, "FUSION [N={CacheName} I={CacheInstanceId}] (O={CacheOperationId} K={CacheKey}): [DC] canceled trying to get entry from distributed", _options.CacheName, _options.InstanceId, operationId, key);
+
+			throw;
 		}
 		catch (Exception exc)
 		{
